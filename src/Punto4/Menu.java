f77b6f350d944cdbc0f3614eaa73d8f44e9687ce
SergioPerez25 <sergioperez250996@gmail.com>
2021-06-16 10:50:49 -0300

    Primera version

--- src/Punto4/Menu.java
+++ src/Punto4/Menu.java
@@ -10,13 +10,12 @@ public class Menu {
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) { 
         int opcion=0;
-        while (opcion!= 3) {
+        while (opcion!= 2) {
             System.out.println("+=========================+");
             System.out.println("+=====  MENU GITHUB  =====+");
             System.out.println("+=========================+");
-            System.out.println("+  1) Saludar             +");
-            System.out.println("+  2) Tateti              +");
-            System.out.println("+  3) Salir del Programa  +");
+            System.out.println("+  1) Saludar (Pérez)     +");
+            System.out.println("+  2) Salir del Programa  +");
             System.out.println("+  Ingrese opcion         +");
             System.out.println("+=========================+");
             opcion=scanner.nextInt();
@@ -25,9 +24,6 @@ public class Menu {
                     System.out.println("Hola mundo con GitHub");
                     break;
                 case 2:
-                    
-                    break;
-                case 3:
                     System.out.println("Saliendo del Programa");
                     System.exit(0);
                     break;
