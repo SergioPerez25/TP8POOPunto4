7009d9625739a78f6064466642254ca02a4f108e
Liz <Liz@noe>
2021-06-17 02:10:26 -0300

    Opcion 3 Noeli Mamani

--- src/Punto4/Menu.java
+++ src/Punto4/Menu.java
@@ -10,13 +10,14 @@ public class Menu {
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) { 
         int opcion=0;
-        while (opcion!= 3) {
+        while (opcion!= 4) {
             System.out.println("+=========================+");
             System.out.println("+=====  MENU GITHUB  =====+");
             System.out.println("+=========================+");
             System.out.println("+  1) Saludar (Pérez)     +");
             System.out.println("+  2) Sumar (S.Mamaní)    +"); //agregado por Selene Mamaní
-            System.out.println("+  3) Salir del Programa  +");
+            System.out.println("+  3) Factorial (N.Mamaní)+"); //agregado por Noeli Mamani
+            System.out.println("+  4) Salir del Programa  +");
             System.out.println("+  Ingrese opcion         +");
             System.out.println("+=========================+");
             opcion=scanner.nextInt();
@@ -34,6 +35,16 @@ public class Menu {
                     System.out.println("El resultado es: "+resultado);
                     break;
                 case 3:
+                    int n;
+                    System.out.println("Ingrese un Numero");
+                    n=scanner.nextInt();
+                    int i=1;
+                    for (int x = 1; x <= n; x++) {
+                        i *= x;
+                    }
+                    System.out.println("El factorial del numero "+n+" es "+i);
+                    break; 
+                case 4:
                     System.out.println("Saliendo del Programa");
                     System.exit(0);
                     break;
